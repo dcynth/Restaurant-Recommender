9437dbddf887dea785156cc5b962e630f7b51fe8
dcynth <30626105+dcynth@users.noreply.github.com>
2021-09-01 15:31:08 -0500

    Delete Downloads/Restaurant-Recommender directory

--- Downloads/Restaurant-Recommender/test/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-	SECRET_KEY = '99be871a4cfdd1da589874f2605cc2eb'
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
-	MAIL_SERVER = 'smtp.googlemail.com'
-	MAIL_PORT = 587
-	MAIL_USE_TLS = True
-	MAIL_USERNAME = 'pricexu96@gmail.com'
-	MAIL_PASSWORD = '123456'
--- Downloads/Restaurant-Recommender/test/users/profiles.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient("mongodb+srv://cs411:project@cluster0-1yfcs.mongodb.net/test?retryWrites=true&w=majority")
-db = client.get_database('users_db')
-records = db.profiles
-print(records.count_documents({}))
